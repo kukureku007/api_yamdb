16c1f01bce44e82fc28d8b54c80bb850ab0fe894
Michael Rudy <michaelrudy@192.168.1.5>
2022-01-31 23:07:44 +0300

    add django extension app

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -2,4 +2,32 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    # USER = 'user'
+    # MODERATOR = 'moderator'
+    # ADMIN = 'admin'
+    # ROLE_CHOICES = (
+    #     (USER, 'user'),
+    #     (MODERATOR, 'moderator'),
+    #     (ADMIN, 'admin'),
+    # )
+    pass
+
+
+class Title():
+    pass
+
+
+class Category():
+    pass
+
+
+class Genre():
+    pass
+
+
+class Review():
+    pass
+
+
+class Comment():
     pass
