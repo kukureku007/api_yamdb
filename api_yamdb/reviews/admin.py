41af5f5810398ce6eeaebddb2a69b9f172492cda
Michael Rudy <michaelrudy@192.168.1.5>
2022-02-01 20:25:53 +0300

    custom user model

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'username',
+        'email',
+        'role',
+        'is_staff',
+        'is_superuser',
+        'date_joined'
+    )
+    list_filter = ('date_joined',)
+    empty_value_display = '-пусто-'
+    list_editable = ('role','is_staff')
+    pass
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,15 +2,22 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    # USER = 'user'
-    # MODERATOR = 'moderator'
-    # ADMIN = 'admin'
-    # ROLE_CHOICES = (
-    #     (USER, 'user'),
-    #     (MODERATOR, 'moderator'),
-    #     (ADMIN, 'admin'),
-    # )
-    pass
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+    ROLE_CHOICES = [
+        (USER, 'user'),
+        (MODERATOR, 'moderator'),
+        (ADMIN, 'admin'),
+    ]
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default=USER,
+    )
+    bio = models.TextField(
+        blank=True,
+    )
 
 
 class Title():
