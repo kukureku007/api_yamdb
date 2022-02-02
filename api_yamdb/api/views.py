dc3d8adc7ad17873426916746317661f8523c952
Michael Rudy <michaelrudy@192.168.1.5>
2022-02-02 23:10:24 +0300

    add temp token auth

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,11 +1,21 @@
 from rest_framework import viewsets
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
+# from rest_framework.decorators import api_view, permission_classes
+# from rest_framework.permissions import AllowAny
+# from rest_framework.response import Response
 
 
-from reviews.models import User, Category, Genre, Title
-from .serializers import TitleSerializer, UserSerializer, CategorySerializer, GenreSerializer
+from reviews.models import (
+    User,
+    Category,
+    Genre,
+    Title
+)
+from .serializers import (
+    TitleSerializer,
+    UserSerializer,
+    CategorySerializer,
+    GenreSerializer
+)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -14,14 +24,14 @@ class UserViewSet(viewsets.ModelViewSet):
     lookup_field = 'username'
 
 
-@api_view(('POST',))
-@permission_classes((AllowAny,))
-def signup(request):
-    
-    if request.method == 'POST':
-        return Response({'message': 'Получены данные', 'data': request.data})
-    
-    return Response({'message': 'Это был GET-запрос!'}) 
+# @api_view(('POST',))
+# @permission_classes((AllowAny,))
+# def signup(request):
+
+#     if request.method == 'POST':
+#         return Response({'message': 'Получены данные', 'data': request.data})
+
+#     return Response({'message': 'Это был GET-запрос!'})
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
