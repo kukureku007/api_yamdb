097bbbcb372184ca65a426ef431550dd2158d613
Bogdan <bogdancharaev@yandex.ru>
2022-02-07 07:18:32 +0300

    passed

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -6,7 +6,8 @@ class TitlesFilter(django_filters.FilterSet):
 
     genre = django_filters.CharFilter(field_name='genre__slug', lookup_expr='icontains')
     category = django_filters.CharFilter(field_name='category__slug', lookup_expr='icontains')
+    name = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Title
-        fields = ('name', 'year', 'genre', 'category')
+        fields = ('name', 'year', 'description', 'genre', 'category')
