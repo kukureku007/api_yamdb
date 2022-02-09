8c1aa7154f05fe217a4a8ae3b9ed661e3e1cf2c7
Bogdan <bogdancharaev@yandex.ru>
2022-02-09 22:04:14 +0300

    sort

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, MaxValueValidator
-from django.db import models
 from datetime import datetime
+
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 
 
 class User(AbstractUser):
