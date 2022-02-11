b7f9823fd5e0a7a2394c0c0aa93086d968d8796e
Bogdan <bogdancharaev@yandex.ru>
2022-02-11 10:49:43 +0300

    fx

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,8 +1,6 @@
-from datetime import datetime
-
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
 from .validators import validate_year
 
 
--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,10 +1,11 @@
-from django.core.exceptions import ValidationError
 from datetime import datetime
+
+from django.core.exceptions import ValidationError
 
 
 def validate_year(value):
     if value > datetime.now().year:
         raise ValidationError('Год выпуска произведения превышает текущий.')
 
-    if value < 1900 :
+    if value < 1900:
         raise ValidationError('Выберите произведения, изданные после 1900г.')
